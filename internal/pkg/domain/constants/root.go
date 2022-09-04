168f6b6f5411f24d5d0a985e93450351a8a11129
Valerio Belli <belli.valerio@outlook.com>
2022-09-04 16:39:14 +0200

    Add Milestones' states as constants

--- internal/pkg/domain/constants/root.go
+++ internal/pkg/domain/constants/root.go
@@ -1,3 +1,11 @@
 package constants
 
 const DateFormat = "2006-01-02"
+
+const (
+	MilestoneStateAll    string = "ALL"
+	MilestoneStateClosed string = "CLOSED"
+	MilestoneStateOpen   string = "OPEN"
+)
+
+var MilestoneStates = []string{MilestoneStateAll, MilestoneStateClosed, MilestoneStateOpen}
