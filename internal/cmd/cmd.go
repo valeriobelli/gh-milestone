c9fe5cf67b69f8d629c1b0fc91a717c033bd92c6
Valerio Belli <belli.valerio@outlook.com>
2023-03-08 23:35:26 +0100

    Bump semantic version at v2.1.0

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -8,7 +8,7 @@ import (
 	"github.com/valeriobelli/gh-milestone/internal/pkg/utils/cmdutil"
 )
 
-const version = "v2.0.1"
+const version = "v2.1.0"
 
 func Execute() {
 	var rootCommand = &cobra.Command{
